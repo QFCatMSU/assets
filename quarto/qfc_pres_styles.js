69f2055ba4ac5bb0ae2da45c484283a43ec47a94
belinskyc <36079242+belinskyc@users.noreply.github.com>
2025-06-02 16:55:51 -0400

    Printer link fixes

--- quarto/qfc_pres_styles.js
+++ quarto/qfc_pres_styles.js
@@ -1,8 +1,11 @@
-
-
 document.addEventListener('DOMContentLoaded', function() 
 {
-    // Only run if we're in print-pdf mode
+  const params = new URLSearchParams(window.location.search);
+  if (params.get("print-pdf") && params.get("filename")) {
+    document.title = params.get("filename");
+  }
+  
+  // Only run if we're in print-pdf mode
   if (window.location.search === "?print-pdf") {
     window.addEventListener("load", () => {
       // Give the browser time to finish rendering
@@ -15,26 +18,37 @@ document.addEventListener('DOMContentLoaded', function()
   else
   {
     subtitle = document.querySelector("p.subtitle");
+    title = document.querySelector("h1.title");
+    
+    if (subtitle)
+      printText = subtitle.innerText;
+    else if (title)
+      printText = title.innerText;
+    else
+      printText = "quarto_doc"
+    
+    document.title = printText;
     
     // create printer icon 
   	printLink = document.createElement('a');
   	printLink.classList.add("self");
   	printLink.target = "_self";
   	printLink.href = "#"
-    printLink.addEventListener("click", function() 
+  	printLink.addEventListener("click", function() 
     {
       const url = new URL(window.location.href);
       url.search = "?print-pdf"; // Trigger Reveal.js print mode
-          window.open(url.toString(), "_blank");
-      //window.location.href = url.toString(); // Reload with new query
+      url.searchParams.set("print-pdf", "1");
+      url.searchParams.set("filename", printText);
+      window.open(url.toString(), "_blank");
     });
     printLink.style.marginLeft = "9px";
   	printLink.innerHTML = "&#9113";
-  	
-  	// stop Quarto from removing the primary header
-  	// titleObj.classList.remove("d-none");
     
-  	// add printer icon to title
-  	subtitle.appendChild(printLink);
+  	// add printer icon to the subtitle or the title (better to add in corner?)
+  	if(subtitle)
+  	  subtitle.appendChild(printLink);
+  	else if (title)
+  	  title.appendChild(printLink);
   }
 })
