a4cd7bf11ba9dd1b4a7f98b40bbfe876787d5065
belinskyc <belinsky@msu.edu>
2025-06-03 14:51:03 -0400

    Updated pres_style.js to remove printer link in print mode

--- quarto/qfc_pres_styles.js
+++ quarto/qfc_pres_styles.js
@@ -1,40 +1,63 @@
-
-
 document.addEventListener('DOMContentLoaded', function() 
 {
-    // Only run if we're in print-pdf mode
-  if (window.location.search === "?print-pdf") {
-    window.addEventListener("load", () => {
-      // Give the browser time to finish rendering
-      setTimeout(() => {
-        window.print();
-      }, 1000); // 1 second is usually enough
+  // Check for print-pdf mode
+  if (window.location.search.match(/print-pdf/gi)) 
+  {
+    // Hide printer link
+    const printLink = document.getElementById("printLink");
+    if (printLink) printLink.style.display = "none";
+    
+    const params = new URLSearchParams(window.location.search);
+    
+    // If the is a filename, set that as the save name for the file 
+    if (params.get("filename")) 
+    {
+      document.title = params.get("filename");
+    }
+  
+    // when revealjs says the pdf is ready, bring up a print dialog
+    Reveal.addEventListener('pdf-ready', () => 
+    {
+      window.print(); 
     });
+
   }
   
   else
   {
     subtitle = document.querySelector("p.subtitle");
+    title = document.querySelector("h1.title");
+    
+    if (subtitle)
+      printText = subtitle.innerText;
+    else if (title)
+      printText = title.innerText;
+    else
+      printText = "quarto_doc"
+    
+    document.title = printText;
     
     // create printer icon 
   	printLink = document.createElement('a');
   	printLink.classList.add("self");
   	printLink.target = "_self";
   	printLink.href = "#"
-    printLink.addEventListener("click", function() 
+  	printLink.id = "printLink"
+  	printLink.addEventListener("click", function() 
     {
       const url = new URL(window.location.href);
       url.search = "?print-pdf"; // Trigger Reveal.js print mode
-          window.open(url.toString(), "_blank");
-      //window.location.href = url.toString(); // Reload with new query
+      url.searchParams.set("print-pdf", "1");
+      url.searchParams.set("filename", printText);
+      window.open(url.toString(), "_blank");
     });
     printLink.style.marginLeft = "9px";
   	printLink.innerHTML = "&#9113";
-  	
-  	// stop Quarto from removing the primary header
-  	// titleObj.classList.remove("d-none");
     
-  	// add printer icon to title
-  	subtitle.appendChild(printLink);
+  	// add printer icon to the subtitle or the title (better to add in corner?)
+  	if(subtitle)
+  	  subtitle.appendChild(printLink);
+  	else if (title)
+  	  title.appendChild(printLink);
   }
 })
