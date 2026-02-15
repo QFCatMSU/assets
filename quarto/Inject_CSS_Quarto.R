cc398aff6428b38d721b2bccaf0d141f785d4ca3
belinskyc <belinsky@msu.edu>
2026-02-15 18:49:24 -0500

    Inject CSS now check for OS and if there was a previous injection

--- quarto/Inject_CSS_Quarto.R
+++ quarto/Inject_CSS_Quarto.R
@@ -2,15 +2,24 @@
 #    - test if this works in MAc and Linux
 #    - color changes do not seem to be working in light mode
 #    - delete old changes before adding new changes
-
-
+#    - put injection at end of CSS?  Makes it more likely to be executed without !important
 
 # Define file paths
 url <- "https://raw.githubusercontent.com/QFCatMSU/assets/refs/heads/main/quarto/css_inject.css"
 
 # Base Positron extensions directory (works for any Windows user)
-base_dir <- file.path(Sys.getenv("USERPROFILE"),
-                      ".positron", "extensions")
+# base_dir <- file.path(Sys.getenv("USERPROFILE"),
+#                       ".positron", "extensions")
+
+if (.Platform$OS.type == "windows") 
+{
+  home_root <- Sys.getenv("USERPROFILE")
+}else
+{
+  home_root <- path.expand("~")
+}
+
+base_dir <- file.path(home_root, ".positron", "extensions")
 
 # Find the installed Quarto extension directory
 quarto_dir <- list.dirs(base_dir,
@@ -27,10 +36,26 @@ target_file = paste0(quarto_dir, "/assets/www/editor/style.css")
 
 # Read contents
 text_prepend <- readLines(url, warn = FALSE)
+inject_msg = "/* Injected by Inject_CSS_Quarto.R */"
+text_prepend2 = c(text_prepend, inject_msg)
+
+# Message to add at end of injection
 text_target  <- readLines(target_file, warn = FALSE)
 
+# Find if the inject_msg already exists
+inject_msg_exists = which(text_target == inject_msg)
+
+# Get last line with inject_msg (should only be one, but...)
+if(length(inject_msg_exists) > 0)
+{
+  inject_line = inject_msg_exists[length(inject_msg_exists)]
+  
+  # delete lines in text_target until (and including) the inject msg
+  text_target = text_target[(inject_line+1):length(text_target)]
+}
+
 # Combine (prepend first file to beginning of second)
-combined_text <- c(text_prepend, text_target)
+combined_text <- c(text_prepend2, text_target)
 
 # Write back to target file
 writeLines(combined_text, target_file)
